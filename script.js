4d3c7468f00d48b3b35d38902685747449342703
Hiroki Kato <hirokikato@Hirokis-MacBook-Air.local>
2025-07-02 17:19:00 -0400

    final fix

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ function onOrientationChange(event) {
     let angle = event.beta - 90;
     if(angle < 0) angle = 0;  // Rotation around the x-axis
     console.log(`Beta: ${angle}`);
-    const distanceToTreeMeters=document.getElementById('mySlider').value;
-    document.getElementById('myLabel').innerText = `Distance to tree: ${distanceToTreeMeters} m`;
-    const height = Math.tan(angle*Math.PI/180)* distanceToTreeMeters;
+    const distanceToObjectMeters=document.getElementById('mySlider').value;
+    document.getElementById('myLabel').innerText = `Distance to object: ${distanceToObjecteMeters} m`;
+    const height = Math.tan(angle*Math.PI/180)* distanceToObjectMeters;
     document.getElementById('heightInfo').innerText = `${height.toFixed(1)} m (${angle.toFixed(1)}°)`;
 }

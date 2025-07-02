8ba24735e08f0278916ae13ecb3b8565a0f37aa9
Hiroki Kato <hirokikato@Hirokis-MacBook-Air.local>
2025-07-02 16:28:47 -0400

    updated play button for iphone permissions

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function main(){
     window.addEventListener('deviceorientation', onOrientationChange)
 
-
-    //front camera
     navigator.mediaDevices.getUserMedia({ video: {facingMode: 'environment'} })
-        .then((signal)=> {
+        .then(function(signal) {
             const video = document.getElementById('myVideo');
             video.srcObject = signal;
             video.play();
         })
         .catch((err) => {  
+            alert('Error accessing camera: ' + err.message);
             console.error('Error accessing camera:', err);
         })
 }
@@ -19,11 +18,8 @@ function onOrientationChange(event) {
     let angle = event.beta - 90;
     if(angle < 0) angle = 0;  // Rotation around the x-axis
     console.log(`Beta: ${angle}`);
-
     const distanceToTreeMeters=document.getElementById('mySlider').value;
-    document.getElementById('myLabel').innerText = `Distance to tree: ${distanceToTreeMeters} m`;
-    
-    
+    document.getElementById('myLabel').innerText = `Distance to tree: ${distanceToTreeMeters} m`;  
     const height = Math.tan(angle*Math.PI/180)* distanceToTreeMeters;
     document.getElementById('heightInfo').innerText = `${height.toFixed(1)} m (${angle.toFixed(1)}°)`;
 }
